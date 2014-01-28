a9a7ead5c45ee3fcc2c6324ad05dd57f7243b2a0
Chris Alley <clalley@gmail.com>
2014-01-28 13:09:04 -0600

    Added verbiage to home page and Procfile for local testing

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: target/start -Dhttp.port=${PORT} -Dconfig.file=prod.conf ${JAVA_OPTS}
--- target/scala-2.10/src_managed/main/views/html/start.template.scala
+++ target/scala-2.10/src_managed/main/views/html/start.template.scala
@@ -40,12 +40,18 @@ Seq[Any](format.raw/*1.26*/("""
 			<h1><center>Start the Holmes Motors Buy Now Application</center></h1>
 			"""),_display_(Seq[Any](/*11.5*/helper/*11.11*/.form(action = routes.BuyNowMain.newBuyNowForm())/*11.60*/ {_display_(Seq[Any](format.raw/*11.62*/("""
 			<center><button type="submit" name="submitButton" class="btn btn-primary" value="StartHere">Start Here</button></center>
+			<h1><center></center></h1>
+			<h1><center></center></h1>
+			<h1><center>NO CREDIT CHECK</center></h1>
+			<h1><center>NO CONTRACT</center></h1>
+			<h1><center>2.9% FINANCING FOR ALL</center></h1>
+			<h1><center>NO REJECTIONS WITH VALID DL AND DOWN PAYMENT </center></h1>
 		</span></div>
    		<div class="right"></div>
 	</div>
 
 
-""")))})),format.raw/*18.2*/("""
+""")))})),format.raw/*24.2*/("""
    
    
 </body>"""))}
@@ -60,11 +66,11 @@ Seq[Any](format.raw/*1.26*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Wed Jan 22 21:50:19 CST 2014
+                    DATE: Tue Jan 28 13:03:07 CST 2014
                     SOURCE: C:/Users/Jason/Documents/buyNow-Prod/app/views/start.scala.html
-                    HASH: 4ec59aa4fe1c9870e38cea19f21114a76b5b8354
-                    MATRIX: 753->1|906->25|934->80|1117->228|1131->234|1193->275|1320->367|1335->373|1393->422|1433->424|1654->614
-                    LINES: 27->1|33->1|34->4|39->9|39->9|39->9|41->11|41->11|41->11|41->11|48->18
+                    HASH: ebebd753d442085636e48d51c1a42c847bb5bf22
+                    MATRIX: 753->1|906->25|934->80|1117->228|1131->234|1193->275|1320->367|1335->373|1393->422|1433->424|1933->893
+                    LINES: 27->1|33->1|34->4|39->9|39->9|39->9|41->11|41->11|41->11|41->11|54->24
                     -- GENERATED --
                 */
             
